ef619ab9973c2374e4f8ba718a3c579551246d85
Andrii Rodzyk <rodzyk@programmer.net>
2024-07-24 11:52:56 +0300

    fix routes pathMatch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: ':id',
-        loadChildren: () => import('./title/title.module').then(m => m.TitleModule)
+        loadChildren: () => import('./title/title.module').then(m => m.TitleModule),
+        pathMatch: 'full'
     },
     {
         path: '',
-        loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule)
+        loadChildren: () => import('./catalog/catalog.module').then(m => m.CatalogModule),
+        pathMatch: 'full'
     },
 ];
