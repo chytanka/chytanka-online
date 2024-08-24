a243be8e262aa6cd43d5e42b36f220253b231a85
Andrii Rodzyk <rodzyk@programmer.net>
2024-08-24 09:16:51 +0300

    update meta og

--- src/app/shared/data-access/meta-tags.service.ts
+++ src/app/shared/data-access/meta-tags.service.ts
@@ -14,17 +14,13 @@ export class MetaTagsService {
   setTitle(title: string) {
     this.title.setTitle(title)
 
-    this.meta.updateTag({
-      name: 'title',
-      content: title
-    })
+    this.meta.updateTag({ name: 'title', content: title })
+    this.meta.updateTag({ property: 'og:title', content: title })
   }
 
   setDesc(desc: string) {
-    this.meta.updateTag({
-      name: 'description',
-      content: desc
-    })
+    this.meta.updateTag({ name: 'description', content: desc })
+    this.meta.updateTag({ property: 'og:description', content: desc })
   }
 
   removeAdult() {
@@ -38,9 +34,13 @@ export class MetaTagsService {
 
   setImage(src: string, text: string) {
     const metaImageHost = `https://chytanka-meta-image.onrender.com`
-    this.meta.updateTag({
-      property: 'og:image',
-      content: `${metaImageHost}/generate-image?imageSrc=${Base64.toBase64(src)}&text=${text}`
-    })
+    const imageUrl = `${metaImageHost}/generate-image?imageSrc=${Base64.toBase64(src)}&text=${text}`
+    this.meta.updateTag({ property: 'og:image', content: imageUrl })
+    this.meta.updateTag({ property: 'og:image:url', content: imageUrl })
+    this.meta.updateTag({ property: 'og:image:secure_url', content: imageUrl })
+    this.meta.updateTag({ property: 'og:image:width', content: '1200' })
+    this.meta.updateTag({ property: 'og:image:height', content: '630' })
+    this.meta.updateTag({ property: 'og:image:type', content: 'image/png' })
+    this.meta.updateTag({ property: 'og:image:alt', content: text })
   }
 }
